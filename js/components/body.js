624b6efaaff063d0fa11b4db48757a9460d7ea4e
Robert Mulholand <robmulholand@gmail.com>
2015-08-18 16:11:45 -0500

    add site template

---
+++ js/components/body.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Body = React.createClass({
+
+  render: function() {
+    return (
+      <div style={{ "textAlign": "center", "paddingTop": "70px" }}>
+        <h1>Time to hit up a Chicago beach?</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = Body;
